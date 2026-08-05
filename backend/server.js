fa10596be510663cc0f717c12ef802effffa17ef
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-08-06 01:11:09 +0500

    Signup flow completed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import { setServers } from "node:dns/promises";
-import memberModel from "./models/member.js";
+import MemberModel from "./models/member.js";
 
 setServers(["8.8.8.8", "1.1.1.1"]);
 
@@ -13,12 +13,12 @@ const PORT = 4000;
 
 mongoose.connect(URI).then((res) => console.log("MongoDB connected"));
 
-// Server Create
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Routes
 
 app.get("/", (request, response) => {
   response.json({
@@ -29,31 +29,52 @@ app.get("/", (request, response) => {
 });
 
 app.post("/api/signup", async (request, response) => {
+  const { fullName, email, password } = request.body;
 
-    const {fullName , email , password} = request.body
+  if (!fullName || !email || !password) {
+    response.json({
+      message: "Required fields are missing",
+      body: null,
+      status: false,
+    });
+    return;
+  }
 
-    if(!fullName , !email , !password){
-response.json(
-    {
-        message : "Required fields are missing",
-        status : false
-    }
-)
-return
-    }
+  const hashPassword = await bcrypt.hash(password, 10);
+  console.log(hashPassword);
 
-    
-    
-  const userData = await memberModel.create(request.body);
-  console.log(userData);
+  const obj = {
+    ...request.body,
+    password: hashPassword,
+  };
 
-  console.log(request.body);
+  //Email Already Exist
+  const emailExist = await MemberModel.findOne({ email });
+  console.log(emailExist);
+
+  if (emailExist) {
+    response.json({
+      message: "Email already exist",
+      body: null,
+      status: false,
+    });
+    return;
+  }
+
+  const userData = await MemberModel.create(obj);
+  // console.log(userData);
+
+  // console.log(obj);
 
   response.json({
     message: "User Created",
+    body: null,
+    status: true,
   });
-
-  
 });
 
-app.listen(PORT, () => console.log("Server running.."));
+// Server running and listening
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
